f3ed6917e9e4702fe40216542cfb8b66ac8e8ffd
shrubbry <shrubbry@gmail.com>
2025-06-09 15:49:40 -0700

    Update check_promotions.py

--- check_promotions.py
+++ check_promotions.py
@@ -1,6 +1,8 @@
 import requests
 import datetime
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from utils import send_email  # assumes working Brevo integration
 
 TODAY = datetime.date.today()
